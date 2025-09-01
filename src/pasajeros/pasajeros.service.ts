eb06d891cd3458986644c03fc55295eacc50e869
Esteban Ahumada <esteban.manuel.ahumada@gmail.com>
2025-09-01 13:44:04 -0400

    Módulo de pasajeros

--- src/pasajeros/pasajeros.service.ts
+++ src/pasajeros/pasajeros.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePasajeroDto } from './dto/create-pasajero.dto';
-import { UpdatePasajeroDto } from './dto/update-pasajero.dto';
+import { Pasajero } from './entities/pasajero.entity';
 
 @Injectable()
 export class PasajerosService {
+
+  private pasajeros: Pasajero[] = [];
+  private id = 1;
+
   create(createPasajeroDto: CreatePasajeroDto) {
-    return 'This action adds a new pasajero';
+    const existe = this.pasajeros.find((a) => a.email === createPasajeroDto.email);
+    if (existe) {
+      throw new BadRequestException(
+        `Ya existe un pasajero con el email "${createPasajeroDto.email}"`,
+      );
+    }
+    const pasajero = new Pasajero(this.id++, createPasajeroDto.nombre, createPasajeroDto.apellido, createPasajeroDto.email);
+    this.pasajeros.push(pasajero);
+    return pasajero;
   }
 
   findAll() {
-    return `This action returns all pasajeros`;
+    return this.pasajeros;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} pasajero`;
-  }
-
-  update(id: number, updatePasajeroDto: UpdatePasajeroDto) {
-    return `This action updates a #${id} pasajero`;
+    const pasajeroEncontrado = this.pasajeros.find(a => a.id === id);
+    if (!pasajeroEncontrado) {
+      throw new NotFoundException(`Pasajero con id ${id} no encontrado`);
+    }
+    return pasajeroEncontrado;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} pasajero`;
+    const index = this.pasajeros.findIndex(p => p.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Pasajero con id ${id} no encontrado`);
+    }
+
+    const eliminado = this.pasajeros.splice(index, 1); 
+    return eliminado;
   }
+
 }
+
+
+
